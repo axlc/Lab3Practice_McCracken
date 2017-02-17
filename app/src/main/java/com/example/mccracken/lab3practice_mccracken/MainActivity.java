7e5087c2b826c0c5e9f9f168f03c6b7758ccdddd
axlc <axl.mccacken@gmail.com>
2017-02-17 14:00:50 -0600

    Implement saving state of application

--- app/src/main/java/com/example/mccracken/lab3practice_mccracken/MainActivity.java
+++ app/src/main/java/com/example/mccracken/lab3practice_mccracken/MainActivity.java
@@ -3,11 +3,57 @@ package com.example.mccracken.lab3practice_mccracken;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String tag = "AJM";
+
+    EditText input_et;
+    TextView output_tv;
+    Button calculate_b;
+
+    private static String answer;
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState)
+    {
+        // The Bundle provides a way of storing a NVP ("Name-Value Pair") map.
+        // It will get passed in to onRestoreInstanceState() OR onCreate()
+        Log.i(tag, "onSaveInstanceState: answer = [" + answer + "]");
+        Toast.makeText(this, "onSaveInstanceState = [" + answer + "]", Toast.LENGTH_SHORT).show();
+
+        savedInstanceState.putString("answer", answer);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        answer = savedInstanceState.getString("answer");
+        Toast.makeText(this, "onSaveInstanceState = [" + answer + "]", Toast.LENGTH_SHORT).show();
+        Log.i(tag, "onRestoreInstanceState: answer = [" + answer + "]");
+
+        // this works, but doesn’t restore the textView.  How to fix???
+        output_tv.setText(answer);
+    }
+
+    // NOTE:  import android.view.View.OnClickListener!!!
+    // NOTE:  OnClickListener is an interface
+    private View.OnClickListener onClickListener = new Button.OnClickListener() {
+        public void onClick(View v) {
+            Log.i(tag, "onClick:");
+
+            answer = answer + input_et.getText().toString();
+
+            output_tv.setText(answer);
+
+            input_et.setText("");
+        }
+    };
 
     @Override
     protected void onPause() {
@@ -60,6 +106,13 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i(tag, "onCreate:");
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
-    }
 
+        input_et    = (EditText) findViewById(R.id.input_et);
+        output_tv   = (TextView) findViewById(R.id.output_tv);
+        calculate_b = (Button)   findViewById(R.id.calculate_b);
+
+        answer = "";
+
+        calculate_b.setOnClickListener(onClickListener);
+    }
 }
